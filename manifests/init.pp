95ac07c4e13241aaee09d058c9626354ec543a61
tinytub <zhaopeng@gmail.com>
2013-07-24 17:52:48 +0800

    add localip, get from facter

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@
 #
 #
 class vodfuturetv {
-	$localip = 	$ipaddress_eth0			#通过facter获取
+	$localip = $ipaddress_eth0			#通过facter获取
 	$gateway = 				#内网网关
 	$webclientip = 			#nginx客户端挂载ip
 	$lo0ip = 				#lo0的vip
